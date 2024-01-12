d25692ae0671ad17736e91e2ac6eb3cee4166cbe
Anna <annakristensen002@gmail.com>
2024-01-12 02:33:05 +0100

    fx:id assigned

--- src/main/java/com/example/mediaplayer/HelloApplication.java
+++ src/main/java/com/example/mediaplayer/HelloApplication.java
@@ -2,17 +2,21 @@ package com.example.mediaplayer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(fxmlLoader.load(), 800, 1280);
+        stage.setTitle("Mediaplayer");
         stage.setScene(scene);
         stage.show();
     }

c5998593bf26f74b32b3e4d498c8b1760c4823c5
Saagaard <85276934+Saagaard@users.noreply.github.com>
2024-01-23 10:12:44 +0100

    ja tjak det så vigtigt

--- src/main/java/com/example/mediaplayer/HelloController.java
+++ src/main/java/com/example/mediaplayer/HelloController.java
@@ -11,6 +11,10 @@ import javafx.scene.media.MediaView;
 
 import java.io.File;
 import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
@@ -43,6 +47,7 @@ public class HelloController implements Initializable {
 
     private MediaPlayer mp;
     private Media me;
+    private static Connection connection;
     boolean isPlaying = false;
 
     /**
@@ -52,25 +57,70 @@ public class HelloController implements Initializable {
      * @param resources
      */
     public void initialize(URL location, ResourceBundle resources){
-        // Build the path to the location of the media file
-        String path = new File("src/main/media/dontstopme.mp4").getAbsolutePath();
-        // Create new Media object (the actual media content)
-        me = new Media(new File(path).toURI().toString());
-        // Create new MediaPlayer and attach the media to be played
-        mp = new MediaPlayer(me);
-        mediaV.setMediaPlayer(mp);
+        // creating connection String
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=dbMp4";
 
-        // Set fitWidth and fitHeight to the AnchorPane's width and height
-        mediaV.fitWidthProperty().bind(((Pane) mediaV.getParent()).widthProperty());
-        mediaV.fitHeightProperty().bind(((Pane) mediaV.getParent()).heightProperty());
+        Properties properties = new Properties();
 
-        mp.setAutoPlay(false);
-        volumeSlider.setValue(mp.getVolume() * 100);
-        volumeSlider.valueProperty().addListener(observable -> {
-            mp.setVolume(volumeSlider.getValue() / 100);
-            int volumePercentValue = (int) volumeSlider.getValue();
-            volumeLabel.setText(volumePercentValue + "%");
-        });
+        properties.setProperty("user", "sa");
+        properties.setProperty("password", "1234");
+        properties.setProperty("encrypt", "false");
+
+        // create connection
+        try {
+            connection = DriverManager.getConnection(url, properties);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareCall(
+                    "SELECT tblPlaylistContent.fldPlaylistOrder, tblMedia.fldFilePath FROM tblPlaylistContent JOIN tblMedia ON tblPlaylistContent.fldMediaId = tblMedia.fldMediaId WHERE tblPlaylistContent.fldPlaylistId = 1 ORDER BY tblPlaylistContent.fldPlaylistOrder ");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Create a list to hold Media objects
+            List<Media> playlist = new ArrayList<>();
+
+            // Iterate over the result set and add Media objects to the playlist
+            while (resultSet.next()) {
+                String filePath = resultSet.getString("fldFilePath");
+
+                // Build the path to the location of the media file
+                String absolutePath = new File(filePath).getAbsolutePath();
+
+                // Create new Media object (the actual media content)
+                Media media = new Media(new File(absolutePath).toURI().toString());
+
+                playlist.add(media);
+            }
+
+            // Create new MediaPlayer
+            mp = new MediaPlayer(playlist.get(0));
+            mediaV.setMediaPlayer(mp);
+
+// Set fitWidth and fitHeight to the AnchorPane's width and height
+            mediaV.fitWidthProperty().bind(((Pane) mediaV.getParent()).widthProperty());
+            mediaV.fitHeightProperty().bind(((Pane) mediaV.getParent()).heightProperty());
+
+            mp.setAutoPlay(false);
+
+// Set up volume slider and label
+            volumeSlider.setValue(mp.getVolume() * 100);
+            volumeSlider.valueProperty().addListener(observable -> {
+                mp.setVolume(volumeSlider.getValue() / 100);
+                int volumePercentValue = (int) volumeSlider.getValue();
+                volumeLabel.setText(volumePercentValue + "%");
+            });
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
     @FXML
@@ -98,5 +148,27 @@ public class HelloController implements Initializable {
     {
         mp.stop();
         isPlaying = false;
+    }
+
+    public static void printDbPlaylist() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareCall("SELECT * FROM tblPlaylistContent JOIN tblMedia ON tblPlaylistContent.fldMediaId = tblMedia.fldMediaId JOIN tblAuthor ON TblMedia.fldAuthorId = tblAuthor.fldAuthorId WHERE tblPlaylistContent.fldPlaylistId = 1 ORDER BY tblPlaylistContent.fldPlaylistOrder");
+
+        System.out.println("Playliste:");
+        try {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int column1Value = resultSet.getInt("fldPlaylistOrder");
+                String column2Value = resultSet.getString("fldTitle").trim();
+                String column3Value = resultSet.getString("fldAuthorName");
+
+                System.out.println("" + column1Value + ". " + column2Value + " by " + column3Value);
+            }
+            resultSet.close();
+            System.out.println("");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
